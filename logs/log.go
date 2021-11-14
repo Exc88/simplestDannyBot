4992408a57a085538922953ca5bbfce37ca13a9f
Exc88 <whitehuffman@gmail.com>
2021-11-14 05:21:19 +0300

    last fix

--- logs/log.go
+++ logs/log.go
@@ -1,14 +1,1 @@
-//todo all
 package logs
-
-import "log"
-
-type Errors struct {
-	infoLog error
-	errorLog error
-}
-
-func (e Errors) ShowInfoLog() error{
-	e.infoLog = e.errorLog
-	log.Printf("asd")
-}
